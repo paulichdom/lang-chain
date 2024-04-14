794d189f0d70043953a5d307827f041733376d15
Domagoj <paulic.domagoj@gmail.com>
2024-04-14 09:48:55 +0200

    create prompt templates

--- main.ts
+++ main.ts
@@ -7,6 +7,47 @@ const model = new ChatOpenAI({
   modelName: 'gpt-3.5-turbo-1106',
 });
 
-const response = await model.invoke([new HumanMessage('Wirite a haiku')]);
+const response1 = await model.invoke([new HumanMessage('Wirite a haiku')]);
 
-console.log({ response });
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+
+const prompt = ChatPromptTemplate.fromTemplate(
+  `What are three good names for a company that makes {product}?`
+);
+
+const response2 = await prompt.format({
+  product: 'colorful socks',
+});
+
+const response3 = await prompt.formatMessages({
+  product: 'colorful socks',
+});
+
+import {
+  SystemMessagePromptTemplate,
+  HumanMessagePromptTemplate,
+} from '@langchain/core/prompts';
+
+const promptFromMessages = ChatPromptTemplate.fromMessages([
+  SystemMessagePromptTemplate.fromTemplate(
+    'You are an expert at picking company names.'
+  ),
+  HumanMessagePromptTemplate.fromTemplate(
+    'What are three good names for a company that makes {product}?'
+  ),
+]);
+
+const response4 = await promptFromMessages.formatMessages({
+  product: 'shiny objects',
+});
+
+const promptFromMessages1 = ChatPromptTemplate.fromMessages([
+  ["system", "You are an expert at picking company names."],
+  ["human", "What are three good names for a company that makes {product}?"]
+]);
+
+const response5 = await promptFromMessages1.formatMessages({
+  product: "shiny objects"
+});
+
+console.log({ response5 });
