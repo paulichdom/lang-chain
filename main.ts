64fa65af8f05ca9b546e1c98cd18edfce685b48f
Domagoj <paulic.domagoj@gmail.com>
2024-04-15 08:41:38 +0200

    implement splitting

--- main.ts
+++ main.ts
@@ -108,11 +108,52 @@ const loader = new GithubRepoLoader(
 const docs = await loader.load();
 
 // Peer dependency
-import * as parse from "pdf-parse";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import * as parse from 'pdf-parse';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 
-const loader1 = new PDFLoader("./data/MachineLearning-Lecture01.pdf");
+const loader1 = new PDFLoader('./data/MachineLearning-Lecture01.pdf');
 
 const rawCS229Docs = await loader1.load();
 
-console.log(rawCS229Docs.slice(0, 5));
+// Splitting
+
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+
+const splitter = RecursiveCharacterTextSplitter.fromLanguage('js', {
+  chunkSize: 32,
+  chunkOverlap: 0,
+});
+
+const code = `function helloWorld() {
+  console.log("Hello, World!");
+  }
+  // Call the function
+  helloWorld();`;
+
+const result = await splitter.splitText(code);
+
+import { CharacterTextSplitter } from 'langchain/text_splitter';
+
+const splitter1 = new CharacterTextSplitter({
+  chunkSize: 32,
+  chunkOverlap: 0,
+  separator: ' ',
+});
+
+const result1 = await splitter1.splitText(code);
+
+const splitter2 = RecursiveCharacterTextSplitter.fromLanguage('js', {
+  chunkSize: 64,
+  chunkOverlap: 32,
+});
+
+const result2 = await splitter2.splitText(code);
+
+const splitter3 = new RecursiveCharacterTextSplitter({
+  chunkSize: 512,
+  chunkOverlap: 64,
+});
+
+const splitDocs = await splitter3.splitDocuments(rawCS229Docs);
+
+console.log(splitDocs.slice(0, 5));
