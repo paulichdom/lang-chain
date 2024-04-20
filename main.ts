d227f693989c9a68d61efa09533a0de2525cd7a4
Domagoj <paulic.domagoj@gmail.com>
2024-04-20 11:32:17 +0200

    put it all together

--- main.ts
+++ main.ts
@@ -104,7 +104,92 @@ const chatHistory = [
   new AIMessage(originalAnswer),
 ];
 
-await rephraseQuestionChain.invoke({
+const response = await rephraseQuestionChain.invoke({
   question: 'Can you list them in bullet point form?',
   history: chatHistory,
 });
+
+// Putting it all together
+
+const convertDocsToString1 = (documents: Document[]): string => {
+  return documents.map((document) => `<doc>\n${document.pageContent}\n</doc>`).join("\n");
+};
+
+const documentRetrievalChain1 = RunnableSequence.from([
+  (input) => input.standalone_question,
+  retriever,
+  convertDocsToString1,
+]);
+
+const ANSWER_CHAIN_SYSTEM_TEMPLATE = `You are an experienced researcher, 
+expert at interpreting and answering questions based on provided sources.
+Using the below provided context and chat history, 
+answer the user's question to the best of 
+your ability 
+using only the resources provided. Be verbose!
+
+<context>
+{context}
+</context>`;
+
+const answerGenerationChainPrompt = ChatPromptTemplate.fromMessages([
+  ["system", ANSWER_CHAIN_SYSTEM_TEMPLATE],
+  new MessagesPlaceholder("history"),
+  [
+    "human", 
+    "Now, answer this question using the previous context and chat history:\n{standalone_question}"
+  ]
+]);
+
+await answerGenerationChainPrompt.formatMessages({
+  context: "fake retrieved content",
+  standalone_question: "Why is the sky blue?",
+  history: [
+    new HumanMessage("How are you?"),
+    new AIMessage("Fine, thank you!")
+  ]
+});
+
+import { RunnablePassthrough } from "@langchain/core/runnables";
+
+const conversationalRetrievalChain = RunnableSequence.from([
+  RunnablePassthrough.assign({
+    standalone_question: rephraseQuestionChain,
+  }),
+  RunnablePassthrough.assign({
+    context: documentRetrievalChain1,
+  }),
+  answerGenerationChainPrompt,
+  new ChatOpenAI({ modelName: "gpt-3.5-turbo" }),
+  new StringOutputParser(),
+]);
+
+import { RunnableWithMessageHistory } from "@langchain/core/runnables";
+import { ChatMessageHistory } from "langchain/stores/message/in_memory";
+
+const messageHistory = new ChatMessageHistory();
+
+const finalRetrievalChain = new RunnableWithMessageHistory({
+  runnable: conversationalRetrievalChain,
+  getMessageHistory: (_sessionId) => messageHistory,
+  historyMessagesKey: "history",
+  inputMessagesKey: "question",
+});
+
+const originalQuestion1 = "What are the prerequisites for this course?";
+
+const originalAnswer1 = await finalRetrievalChain.invoke({
+  question: originalQuestion1,
+}, {
+  configurable: { sessionId: "test" }
+});
+
+const finalResult = await finalRetrievalChain.invoke({
+  question: "Can you list them in bullet point form?",
+}, {
+  configurable: { sessionId: "test" }
+});
+
+console.log(finalResult);
+
+
